bcf8a29f369b5cccc0e77dea90db8be21d81cf4f
PoorviTB <poorvitb2006@gmail.com>
2026-09-19 18:06:27 -1200

    Add files via upload

---
+++ study _assistant.py
@@ -0,0 +1,18 @@
+print(" AI Study Assistant")
+
+topic = input("Enter a topic: ").lower()
+
+if topic == "python":
+    print("Python is a programming language used for software development, AI, and automation.")
+
+elif topic == "dsa":
+    print("DSA means Data Structures and Algorithms. It helps us solve programming problems efficiently.")
+
+elif topic == "cybersecurity":
+    print("Cybersecurity is the practice of protecting computers, networks, and data from attacks.")
+
+elif topic == "ai":
+    print("Artificial Intelligence is the field of creating systems that can perform tasks requiring human-like intelligence.")
+
+else:
+    print("Sorry, I don't have information about this topic yet.")
